b70b9bc6908a49feb85fbbe0a03e94a12838b139
Raycoms <ray.neiheiser@gmail.com>
2023-04-22 16:21:36 +0200

    Revert "jitter again"
    This reverts commit 40a3713c3e452b3959561cfa802be8269773b186.

--- crates/transaction-emitter-lib/src/emitter/mod.rs
+++ crates/transaction-emitter-lib/src/emitter/mod.rs
@@ -338,9 +338,7 @@ impl EmitJobRequest {
                     transactions_per_account: transactions_per_account
                         .min(num_workers_per_endpoint * clients_count),
                     max_submit_batch_size: DEFAULT_MAX_SUBMIT_TRANSACTION_BATCH_SIZE,
-                    worker_offset_mode: WorkerOffsetMode::Jitter{
-                        jitter_millis: 5000
-                    },
+                    worker_offset_mode: WorkerOffsetMode::Spread,
                     accounts_per_worker: 100,
                     workers_per_endpoint: num_workers_per_endpoint,
                     endpoints: clients_count,
