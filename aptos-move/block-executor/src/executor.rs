c1a955d6a72565514b2b094f9bebebedc91f946a
Raycoms <ray.neiheiser@gmail.com>
2023-04-22 13:38:54 +0200

    unscpec

--- aptos-move/block-executor/src/executor.rs
+++ aptos-move/block-executor/src/executor.rs
@@ -472,7 +472,7 @@ where
             match interm_res {
                 ExtrResult::Error(v) => {
                     maybe_err = Some(v);
-                    println!("Aaaarrrrr {}", maybe_err.unwrap());
+                    println!("Aaaarrrrr");
                     break;
                 }
                 ExtrResult::Value(v) => {
