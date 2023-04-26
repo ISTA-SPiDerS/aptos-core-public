c63c08b6ff3e94f21ea5046540f19fd65b5fe6a8
Raycoms <ray.neiheiser@gmail.com>
2023-04-26 17:36:23 +0200

    none

--- aptos-move/block-executor/src/executor.rs
+++ aptos-move/block-executor/src/executor.rs
@@ -14,7 +14,6 @@ use crate::{
 
 use tracing::info;
 use color_eyre::Report;
-use tracing_subscriber::EnvFilter;
 use itertools::Itertools;
 use rayon::{prelude::*, scope, ThreadPoolBuilder};
 use std::{
@@ -191,9 +190,6 @@ where
         if std::env::var("RUST_LOG").is_err() {
             std::env::set_var("RUST_LOG", "info")
         }
-        tracing_subscriber::fmt::fmt()
-            .with_env_filter(EnvFilter::from_default_env())
-            .init();
 
         Ok(())
     }
