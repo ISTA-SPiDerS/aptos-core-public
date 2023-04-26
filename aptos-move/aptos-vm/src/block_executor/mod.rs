1e9fc3803caf562a037cab4f688f9c8b1f309777
Ray  Neiheiser <ray.neiheiser@gmail.com>
2023-04-26 13:44:21 +0200

    Revert "4"
    This reverts commit 508d68f83410dcc6e599f9d2fb7957de72cbdb09.

--- aptos-move/aptos-vm/src/block_executor/mod.rs
+++ aptos-move/aptos-vm/src/block_executor/mod.rs
@@ -109,9 +109,9 @@ impl BlockAptosVM {
 
         drop(signature_verification_timer);
 
-        BLOCK_EXECUTOR_CONCURRENCY.set(4 as i64);
+        BLOCK_EXECUTOR_CONCURRENCY.set(6 as i64);
         let executor = BlockExecutor::<PreprocessedTransaction, AptosExecutorTask<S>, S>::new(
-            4,
+            6,
         );
 
         let ret = executor
