f5e64afc0fd8382fc2b110b231ffaf0102f21aab
Ray  Neiheiser <ray.neiheiser@gmail.com>
2023-04-27 11:10:16 +0200

    lo

--- mempool/src/core_mempool/filler.rs
+++ mempool/src/core_mempool/filler.rs
@@ -287,7 +287,7 @@ impl<'a, V: TransactionValidation, const C: u64> BlockFiller for DependencyFille
                         None => {
                             let result = self.transaction_validation.speculate_transaction(&tx);
                             (i, match result {
-                                Result::Ok((ref a, ref b)) => past_results.insert(tx.authenticator(), ((a.clone(), b.clone()))),
+                                Result::Ok((ref a, ref b)) => Ok(past_results.insert(tx.authenticator(), ((a.clone(), b.clone()))).unwrap()),
                                 Result::Err(ref e) => {
                                     println!("Error during pre execution {}", e);
                                     Err(anyhow!("Error during pre execution"))
