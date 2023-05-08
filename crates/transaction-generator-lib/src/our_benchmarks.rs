de8e40d8fd14b57037ab5f872da05fa4f78ee665
Ray  Neiheiser <ray.neiheiser@gmail.com>
2023-05-08 14:04:59 +0200

    Revert "undo"
    This reverts commit 02fa9fe1cac8c03713496452c350801fbd94be1a.

--- crates/transaction-generator-lib/src/our_benchmarks.rs
+++ crates/transaction-generator-lib/src/our_benchmarks.rs
@@ -67,7 +67,7 @@ impl TransactionGenerator for OurBenchmark {
         mut accounts: Vec<&mut LocalAccount>,
         transactions_per_account: usize,
     ) -> Vec<SignedTransaction> {
-        let needed = accounts.len();
+        let needed = accounts.len() * transactions_per_account;
         let mut requests = Vec::with_capacity(needed);
         let load_type = self.load_type;
         let coins = COIN_DISTR.len();
