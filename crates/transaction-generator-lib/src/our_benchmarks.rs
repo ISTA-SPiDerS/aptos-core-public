21f351a31b266748b12e4613704e9f69514e6b8b
Ray  Neiheiser <ray.neiheiser@gmail.com>
2023-05-15 12:21:01 +0200

    adjust solana distribution

--- crates/transaction-generator-lib/src/our_benchmarks.rs
+++ crates/transaction-generator-lib/src/our_benchmarks.rs
@@ -3,6 +3,7 @@
 
 use std::any::Any;
 use std::borrow::Borrow;
+use std::cmp::max;
 use super::{
     publishing::{module_simple::EntryPoints, publish_util::Package},
     TransactionExecutor,
@@ -196,7 +197,7 @@ impl TransactionGenerator for OurBenchmark {
                     writes.push(dist.sample(&mut rng) as u64);
                 }
 
-                let length = cost.round() as usize;
+                let length = max(1, cost.round() as usize);
 
                 requests.push(self.package.our_spec_transaction(accounts[idx],
                                                                 &self.txn_factory,
