f012c77d18818500ef746414f37a2bd43f783fb1
Ray  Neiheiser <ray.neiheiser@gmail.com>
2023-06-15 14:47:10 +0200

    12

--- testsuite/forge/src/backend/local/swarm.rs
+++ testsuite/forge/src/backend/local/swarm.rs
@@ -136,7 +136,7 @@ impl LocalSwarm {
             .with_init_config(Some(Arc::new(
                 move |index, config, genesis_stake_amount| {
                     // for local tests, turn off parallel execution:
-                    config.execution.concurrency_level = 8;
+                    config.execution.concurrency_level = 12;
 
                     // Single node orders blocks too fast which would trigger backpressure and stall for 1 sec
                     // which cause flakiness in tests.
