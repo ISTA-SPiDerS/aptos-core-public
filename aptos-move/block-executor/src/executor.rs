92743c93f09ec354ab47aa1ace19f4c0091536ae
Ray  Neiheiser <ray.neiheiser@gmail.com>
2023-06-21 10:35:09 +0200

    move earlier

--- aptos-move/block-executor/src/executor.rs
+++ aptos-move/block-executor/src/executor.rs
@@ -597,10 +597,6 @@ where
         mode: ExecutionMode,
         profiler: &mut Profiler
     ) -> Result<Vec<(E::Output, Vec<(T::Key, WriteOp)>)>, E::Error> {
-
-        //println!("bla estimates {:?}", signature_verified_block.gas_estimates());
-        //println!("bla deps {:?}", signature_verified_block.dependency_graph());
-
         let mut ret = if self.concurrency_level > 1 {
             self.execute_transactions_parallel(
                 executor_arguments,
