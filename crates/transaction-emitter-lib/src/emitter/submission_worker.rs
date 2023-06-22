22c3001885020054251b4e30d71003a0e8ec5b65
Ray  Neiheiser <ray.neiheiser@gmail.com>
2023-06-22 08:58:02 +0200

    2k

--- crates/transaction-emitter-lib/src/emitter/submission_worker.rs
+++ crates/transaction-emitter-lib/src/emitter/submission_worker.rs
@@ -264,7 +264,7 @@ impl SubmissionWorker {
     }
 
     fn gen_requests(&mut self) -> Vec<SignedTransaction> {
-        let batch_size = 1000;
+        let batch_size = 2000;
         let accounts = self
             .accounts
             .iter_mut()
