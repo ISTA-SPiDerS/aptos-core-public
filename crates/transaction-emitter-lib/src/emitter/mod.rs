30646df98a9aa996a4b660b53f6b208148c04642
Ray  Neiheiser <ray.neiheiser@gmail.com>
2023-07-07 13:24:34 +0200

    try this

--- crates/transaction-emitter-lib/src/emitter/mod.rs
+++ crates/transaction-emitter-lib/src/emitter/mod.rs
@@ -131,6 +131,9 @@ pub struct EmitJobRequest {
     txn_expiration_time_secs: u64,
     init_expiration_multiplier: f64,
 
+    accounts_per_worker: usize,
+    workers_per_endpoint:  usize,
+
     init_retry_interval: Duration,
 
     max_transactions_per_account: usize,
@@ -157,6 +160,8 @@ impl Default for EmitJobRequest {
             mint_to_root: false,
             txn_expiration_time_secs: 270,
             init_expiration_multiplier: 3.0,
+            accounts_per_worker: 500,
+            workers_per_endpoint:  20,
             init_retry_interval: Duration::from_secs(60),
             max_transactions_per_account: 10,
             expected_max_txns: MAX_TXNS,
@@ -266,7 +271,7 @@ impl EmitJobRequest {
                 // we can ~3 blocks in consensus queue. As long as we have 3x the target TPS as backlog,
                 // it should be enough to produce the target TPS.
                 let transactions_per_account = self.max_transactions_per_account;
-                let num_workers_per_endpoint = 20;
+                let num_workers_per_endpoint = self.workers_per_endpoint;
 
                 info!(
                     " Transaction emitter target mempool backlog is {}",
@@ -284,7 +289,7 @@ impl EmitJobRequest {
                     transactions_per_account,
                     max_submit_batch_size: DEFAULT_MAX_SUBMIT_TRANSACTION_BATCH_SIZE,
                     worker_offset_mode: WorkerOffsetMode::Spread,
-                    accounts_per_worker: 500,
+                    accounts_per_worker: self.accounts_per_worker,
                     workers_per_endpoint: num_workers_per_endpoint,
                     endpoints: clients_count,
                     check_account_sequence_only_once_fraction: 0.0,
