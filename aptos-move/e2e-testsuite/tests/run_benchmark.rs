f55b722486cbab6c32960ab0b9b8d1083d6c4fd9
Ray  Neiheiser <ray.neiheiser@gmail.com>
2023-07-21 10:14:52 +0200

    bursty

--- aptos-move/e2e-testsuite/tests/run_benchmark.rs
+++ aptos-move/e2e-testsuite/tests/run_benchmark.rs
@@ -123,7 +123,7 @@ fn main() {
 
     for mode in modes {
         for c in core_set {
-            runExperimentWithSetting(mode, COIN_DISTR.len(), c, trial_count, num_accounts, block_size, &mut executor, &module_id, &accounts, &module_owner, &mut seq_num, DEXAVG);
+            runExperimentWithSetting(mode, COIN_DISTR.len(), c, trial_count, num_accounts, block_size, &mut executor, &module_id, &accounts, &module_owner, &mut seq_num, DEXBURSTY);
         }
         println!("#################################################################################");
     }
