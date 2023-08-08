6ddd35cc7fceda7137242b58cb128cd4263fdeaa
Ray  Neiheiser <ray.neiheiser@gmail.com>
2023-08-08 09:39:36 +0200

    pin too

--- types/src/transaction/mod.rs
+++ types/src/transaction/mod.rs
@@ -1688,15 +1688,15 @@ impl<T: Send + Sync + Clone> Into<TransactionRegister<T>> for Vec<T> {
 pub static RAYON_EXEC_POOL: Lazy<rayon::ThreadPool> = Lazy::new(|| {
     rayon::ThreadPoolBuilder::new()
         .num_threads(cmp::min(32, num_cpus::get()))
-        /*.thread_name(|index| format!("par_exec_{}", index))
+        .thread_name(|index| format!("par_exec_{}", index))
         .spawn_handler(|thread| {
             std::thread::spawn(|| {
-                //let core_ids: Vec<core_affinity::CoreId> = core_affinity::get_core_ids().unwrap();
-                //let res = core_affinity::set_for_current(core_ids[thread.index()].clone());
+                let core_ids: Vec<core_affinity::CoreId> = core_affinity::get_core_ids().unwrap();
+                let res = core_affinity::set_for_current(core_ids[thread.index()].clone());
                 thread.run();
             });
             Ok(())
-        })*/
+        })
         .build()
         .unwrap()
 });
