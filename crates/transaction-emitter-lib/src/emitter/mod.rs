78d1349ad50d70fbf2c01438edc29b0d748eea65
Ray  Neiheiser <ray.neiheiser@gmail.com>
2023-09-05 08:59:09 +0200

    300

--- crates/transaction-emitter-lib/src/emitter/mod.rs
+++ crates/transaction-emitter-lib/src/emitter/mod.rs
@@ -160,7 +160,7 @@ impl Default for EmitJobRequest {
             mint_to_root: false,
             txn_expiration_time_secs: 270,
             init_expiration_multiplier: 3.0,
-            accounts_per_worker: 400,
+            accounts_per_worker: 300,
             workers_per_endpoint:  10,
             init_retry_interval: Duration::from_secs(60),
             max_transactions_per_account: 10,
