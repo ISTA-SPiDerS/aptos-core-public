e7a212cf4b3f2af583e9d6dc7a41faabc3e82957
Raycoms <ray.neiheiser@gmail.com>
2024-02-08 08:39:14 +0100

    ugh

--- types/src/transaction/mod.rs
+++ types/src/transaction/mod.rs
@@ -1693,7 +1693,7 @@ impl<T: Send + Sync + Clone> Into<TransactionRegister<T>> for Vec<T> {
 
 pub static RAYON_EXEC_POOL: Lazy<rayon::ThreadPool> = Lazy::new(|| {
     rayon::ThreadPoolBuilder::new()
-        .num_threads(cmp::min(16, num_cpus::get()))
+        .num_threads(cmp::min(64, num_cpus::get()))
         .thread_name(|index| format!("par_exec_{}", index))
         .build()
         .unwrap()
