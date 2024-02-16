f5312c7437ae213747720088c85c1e01ef7cd1da
Raycoms <ray.neiheiser@gmail.com>
2024-02-16 09:49:25 +0100

    reset full cores

--- aptos-move/e2e-testsuite/tests/run_benchmark.rs
+++ aptos-move/e2e-testsuite/tests/run_benchmark.rs
@@ -131,8 +131,8 @@ fn main() {
     // b) Good blocks BlockSTM vs Good blocks BlockSTM (optimistic) = 2
     // c) Varying workload and how we adjust to it.
 
-    //let core_set = [4, 8, 16, 32, 64];
-    let core_set = [4,6,8];
+    let core_set = [4, 8, 16, 32, 64];
+    //let core_set = [4,6,8];
 
     let trial_count = 3;
     let modes = [Pythia_Sig];
