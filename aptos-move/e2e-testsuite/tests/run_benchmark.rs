599f1c253af16936616e5087dee106cc48d2e6e1
Raycoms <ray.neiheiser@gmail.com>
2024-02-27 21:40:16 +0100

    try

--- aptos-move/e2e-testsuite/tests/run_benchmark.rs
+++ aptos-move/e2e-testsuite/tests/run_benchmark.rs
@@ -181,6 +181,27 @@ fn main() {
         }
     }
 
+    for mode in modes {
+        for mode_two in additional_modes {
+            for c in core_set {
+                let mut time = runExperimentWithSetting(mode, c, trial_count, num_accounts, block_size, &mut executor, &module_id, &accounts, &module_owner, &mut seq_num, NFT, 1300000, mode_two);
+                while time == u128::MAX {
+                    time = runExperimentWithSetting(mode, c, trial_count, num_accounts, block_size, &mut executor, &module_id, &accounts, &module_owner, &mut seq_num, NFT, 1300000, mode_two);
+                }
+            }
+            println!("#################################################################################");
+        }
+    }
+
+    for mode in modes {
+        for mode_two in additional_modes {
+            for c in core_set {
+                let mut time = runExperimentWithSetting(mode, c, trial_count, num_accounts, block_size, &mut executor, &module_id, &accounts, &module_owner, &mut seq_num, DEXBURSTY, 1300000, mode_two);
+            }
+            println!("#################################################################################");
+        }
+    }
+
 
     /*for mode in modes {
        for mode_two in additional_modes {
