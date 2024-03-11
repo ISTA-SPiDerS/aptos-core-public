4158c88fab48a8e229a678fc87c1b112b4736deb
Raycoms <ray.neiheiser@gmail.com>
2024-03-11 09:43:52 +0100

    don't log

--- aptos-move/block-executor/src/executor.rs
+++ aptos-move/block-executor/src/executor.rs
@@ -376,9 +376,9 @@ where
         (*profiler.lock()).count("#txns".to_string(), num_txns as u128);
 
         if num_txns > 2 {
-            let mut prof = &(*profiler.lock());
-            prof.collective_times.iter().for_each(|f | println!("bla {}: {}", f.0, f.1.as_millis()));
-            prof.counters.iter().for_each(|f | println!("bla {}: {}", f.0, f.1));
+            //let mut prof = &(*profiler.lock());
+            //prof.collective_times.iter().for_each(|f | println!("bla {}: {}", f.0, f.1.as_millis()));
+            //prof.counters.iter().for_each(|f | println!("bla {}: {}", f.0, f.1));
         }
 
         // TODO: for large block sizes and many cores, extract outputs in parallel.
