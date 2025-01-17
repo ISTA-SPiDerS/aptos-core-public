0f944bb7c183b7a16b5782e7abc116908058dd83
Raycoms <ray.neiheiser@gmail.com>
2025-01-17 09:54:24 +0100

    Revert "1"
    This reverts commit d97b1163dc237afccf4f2c86657cbc220957dd58.

--- aptos-move/e2e-testsuite/tests/run_benchmark.rs
+++ aptos-move/e2e-testsuite/tests/run_benchmark.rs
@@ -73,7 +73,7 @@ const SEQ_NUM: u64 = 10;
 const MAX_COIN_NUM: usize = 1000;
 const CORES: u64 = 10;
 
-const BATCHES: usize = 1;
+const BATCHES: usize = 5;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
